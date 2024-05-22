366668371364b4e848e189ac67b613698db14f18
kuppireddypravalika <pravalikareddykuppireddy4@gmail.com>
2024-05-22 19:15:40 -0400

    second commit

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <h1>Coming Soon!...</h1>
-        
-      </header>
+      <div className="coming-soon">Coming Soon</div>
     </div>
   );
 }
